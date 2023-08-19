4d2a95e24ac4bb48e99287955bd271b8dd7c344d
tobyrushton <tobyjrushton@icloud.com>
2023-08-19 22:53:11 +0100

    eslint and prettier configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import type { FC, ReactElement } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'DressEasy',
-  description: 'An easy way to choose outfits',
+    title: 'DressEasy',
+    description: 'An easy way to choose outfits',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+const RootLayout: FC<{ children: ReactElement }> = ({ children }) => {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
+
+export default RootLayout
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return (<div></div>)
+import type { FC } from 'react'
+
+const Home: FC = () => {
+    return <div />
 }
+
+export default Home
